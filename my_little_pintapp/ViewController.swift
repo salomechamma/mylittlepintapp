211d714ac32aa1e2aeb480218bc754d5b63aa16e
Salome Chamma <Salome@salomes-macbook-air.local>
2017-02-28 20:54:37 -0800

    Initial Commit

---
+++ my_little_pintapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_little_pintapp
+//
+//  Created by Salome Chamma on 2/28/17.
+//  Copyright © 2017 Salome Chamma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
